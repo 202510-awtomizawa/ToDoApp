d509303bc1040f3589e306fd3384ae54fbabc8a4
202510-awtomizawa <202510_aw_tomizawa@x-ide.sakura.ne.jp>
2026-01-30 15:12:54 +0900

    Todoリポジトリを追加

---
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -0,0 +1,16 @@
+package com.example.todo.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.todo.entity.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+  List<Todo> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(
+      String title,
+      String author,
+      Sort sort);
+}
+
